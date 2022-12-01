cfab7ff33c77c92ae01d0e0a8e0a8226f1ad1afd
Florian Wust <florian.wust1@gmail.com>
2022-12-01 17:36:39 +0100

    Day01

--- web/src/Puzzle/PuzzleDay01.php
+++ web/src/Puzzle/PuzzleDay01.php
@@ -6,6 +6,8 @@ use Entity\PuzzleBase;
 
 class PuzzleDay01 extends PuzzleBase {
 
+  private array $elves_calories = [];
+
   /**
    * @inheritDoc
    */
@@ -20,6 +22,26 @@ class PuzzleDay01 extends PuzzleBase {
   public function processPart1() {
     $this->render($this->helper->printPart('one'));
 
+    $elve_n = 1;
+    foreach ($this->input as $input_val) {
+      // New elve, setting calories to 0 to begin
+      if (!array_key_exists($elve_n, $this->elves_calories)) {
+        $this->elves_calories[$elve_n] = 0;
+      }
+
+      if ($input_val === '') {
+        // New elve
+        $elve_n++;
+      } else {
+        $this->elves_calories[$elve_n] += $input_val;
+      }
+
+    }
+
+    $max_elve_calories = max($this->elves_calories);
+
+    $this->render("How many total Calories is that Elf carrying? <br> <strong>$max_elve_calories</strong>");
+
     // Process the second part of the puzzle.
     $this->processPart2();
   }
@@ -30,7 +52,9 @@ class PuzzleDay01 extends PuzzleBase {
   public function processPart2() {
     $this->render($this->helper->printPart('two'));
 
-    
+    asort($this->elves_calories);
+    $result = array_sum(array_slice(array_reverse($this->elves_calories), 0, 3));
+    $this->render("Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total? <br> <strong>$result</strong>");
   }
 
 }
